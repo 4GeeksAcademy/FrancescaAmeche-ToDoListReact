bac3afab902e398342eb3f6da01d2768b5d5b05c
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-09-13 17:38:17 +0000

    x appears when hovering, styles

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 const List = () => {
     const [items, setItems] = useState("");
     const [addToList, setAddToList] = useState([]);
+    const [showOnHover, setShowOnHover] = useState({display: "none"});
 
     function addItemToList(e) {
         if (e.key === "Enter") {
@@ -16,7 +19,7 @@ const List = () => {
                 <h1>To-Do List</h1>
                 <ul>
                     <li><input value={items} type="text" className="form-control" placeholder="To-Do" onChange={(e) => {setItems(e.target.value)}} onKeyDown= {addItemToList}/></li>
-                    {addToList.map ((a, index) => (<li>{a}{""}<button onClick={() => setAddToList(addToList.filter ((t, currentIndex) => index != currentIndex))}>X</button></li>))}
+                    {addToList.map ((a, index) => (<li onMouseEnter={e => {setShowOnHover({display: "block"});}} onMouseLeave={e => {setShowOnHover({display: "none"})}}>{a}{""}<button style={showOnHover} onClick={() => setAddToList(addToList.filter ((t, currentIndex) => index != currentIndex))}><FontAwesomeIcon icon={faX} /></button></li>))}
                 </ul>
                 <p>{addToList.length} items left</p>
             </div>
